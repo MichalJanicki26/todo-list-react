e35fb68b1ab82d5bb5c71cd4f7d7e01a12f84651
MichalJanicki26 <120520212+MichalJanicki26@users.noreply.github.com>
2023-03-06 23:38:35 +0100

    first commit not working

---
+++ src/Header/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const Header = ({ title }) => {
+    <h1 className="head">
+    {title}
+    </h1>
+};
+
+export default Header;
---
+++ src/Tasks/index.js
@@ -0,0 +1,22 @@
+import "./style.css";
+
+const Tasks = ({tasks, hideDoneTasks}) => (
+    <ul  className="tasks">
+        {tasks.map(task => (
+            <li 
+            className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}
+            >
+                    <button className="tasks__checkedButton">
+                    {task.done ? "✓" : ""}
+                    </button>
+                    <span className={`tasks__content${task.done ? "--done" : ""}`}>
+                    {task.content}</span>
+                    <button className="tasks__removeButton">
+                    X
+                    </button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
