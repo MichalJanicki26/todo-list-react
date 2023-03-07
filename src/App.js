0fa294fa98c6cf10ca41d6c1aa94383b807fcf06
MichalJanicki26 <120520212+MichalJanicki26@users.noreply.github.com>
2023-03-07 16:48:05 +0100

    changes in styles and trimimg in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Form from "./Form";
-import Tasks from "./Tasks";
-import Buttons from "./Buttons";
-import Section from "./Section";
+import Container from "./Container";
 import Header from "./Header";
+import Section from "./Section";
+import Form from "./Form";
+import Buttons from "./Buttons";
+import Tasks from "./Tasks";
 
 const tasks = [
-  { id: 1, content: "przejść na Reacta", done: true },
+  { id: 1, content: "przejść na Reacta", done: false },
   { id: 2, content: "zjeść obiad", done: true },
 ];
 
@@ -14,7 +15,7 @@ const hideDoneTasks = false;
 
 function App() {
   return (
-    <main className="container">
+    <Container>
       <Header title="Lista Zadań" />
       <Section
         title="Dodaj nowe zadanie"
@@ -26,7 +27,7 @@ function App() {
         extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
         body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
       />
-    </main>
+    </Container>
   );
 }
 
